f686822c426b55e4f901834dd9eecb9f90fcc9f2
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-03 21:13:18 -0300

    mejore el codigo realice correcciones de codigo, elimini codigo basura.

--- src/datos.ts
+++ src/datos.ts
@@ -1,11 +1,13 @@
-let btnEnv1 = document.getElementById("btnEnviar1");
 let base = document.getElementById("base");
 let rotulo = document.getElementById("rotulo");
-rotulo.innerHTML = "ingrese el valor de Base:";
-let btnEnv2 = document.getElementById("btnEnviar2");
+
 let altura = document.getElementById("altura");
 let rotulo1 = document.getElementById("rotulo1");
+
+rotulo.innerHTML = "ingrese el valor de Base:";
 rotulo1.innerHTML = "ingrese el valor de Altura:";
 
+let btnEnv1 = document.getElementById("btnEnviar1");
 btnEnv1.addEventListener("click", () => {
   console.log("el area del rectangulo", base.value * altura.value);
+});
